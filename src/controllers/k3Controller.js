4277fecf3a2c3ac19ddfff4993659cf61b50e971
SAHILDEWAN23 <sahildewan2323@gmail.com>
2024-06-06 15:50:58 +0530

    hlo

--- src/controllers/k3Controller.js
+++ src/controllers/k3Controller.js
@@ -322,6 +322,10 @@ const addK3 = async (game) => {
         if (game == 5) join = 'k3d5';
         if (game == 10) join = 'k3d10';
 
+<<<<<<< HEAD
+=======
+          console.log("addk3");
+>>>>>>> 02cf980 (hlo)
 
         let result2 = makeid(3);
         let timeNow = Date.now();
